c73e155c6ca3126eab10c83530adc573abbead3d
Sam Fries <fries2@llnl.gov>
2016-11-16 14:03:05 -0800

    Added cert option to publish_metadata

--- publish_metadata.py
+++ publish_metadata.py
@@ -7,6 +7,7 @@ import basejumper
 import sys
 
 parser = args.parser
+parser.add_argument("--cert", nargs=1, help="Certificate Authority to use")
 parser.add_argument("group", help="ESGF group to associate data with")
 a = parser.parse_args()
 config = args.get_config_module(a.config)
@@ -19,6 +20,6 @@ for k in "path", "size", "hash", "hash_function", "modified", "signature":
         print "No %s provided." % k
         sys.exit(1)
 
-r = requests.post(config.daemon_config["BASEJUMP_FRONTEND_URL"] + "/expose/" + a.group, data=data)
+r = requests.post(config.daemon_config["BASEJUMP_FRONTEND_URL"] + "/expose/" + a.group, data=data, verify=a.cert)
 print r.json()["queue_url"]
 
